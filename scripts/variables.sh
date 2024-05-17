641d96ed7b014a762f3ba954fe5cb643b8f1d157
Phil Hindman <pahindman@user.noreply.github.com>
2024-05-17 00:02:06 -0500

    Put default filter (ansifilter) in variable

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -32,6 +32,32 @@ logging_filename=${logging_filename:-$default_logging_filename}
 
 logging_full_filename="${logging_path}/${logging_filename}"
 
+ansifilter_installed() {
+	type ansifilter >/dev/null 2>&1 || return 1
+}
+
+system_osx() {
+	[ $(uname) == "Darwin" ]
+}
+
+get_ansifilter_command() {
+	if ansifilter_installed; then
+		echo "ansifilter"
+	elif system_osx; then
+		# OSX uses sed '-E' flag and a slightly different regex
+		# Warning, very complex regex ahead.
+		# Some characters below might not be visible from github web view.
+		local ansi_codes_osx="(\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]||]0;[^]+|[[:space:]]+$)"
+		echo "sed -E \"s/$ansi_codes_osx//g\""
+	else
+		local ansi_codes="(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]|)"
+		echo "sed -r 's/$ansi_codes//g'"
+	fi
+}
+
+default_logging_filter="$(get_ansifilter_command)"
+logging_filter=${logging_filter:-$default_logging_filter}
+
 # Screen capture options
 default_screen_capture_path="$HOME"
 screen_capture_path=$(tmux show-option -gqv "@screen-capture-path")
@@ -53,3 +79,4 @@ save_complete_history_filename=$(tmux show-option -gqv "@save-complete-history-f
 save_complete_history_filename=${save_complete_history_filename:-$default_save_complete_history_filename}
 
 save_complete_history_full_filename="${save_complete_history_path}/${save_complete_history_filename}"
+
