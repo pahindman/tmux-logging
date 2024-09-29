37e0b39af5705581b7acb2a4fbe954c6779201b6
Phil Hindman <pahindman@user.noreply.github.com>
2024-09-28 23:34:12 -0500

    Simplify check_tmux_version.sh
    - Remove extraneous echo commands.
    - Consolidate remaining logic

--- scripts/check_tmux_version.sh
+++ scripts/check_tmux_version.sh
@@ -9,32 +9,15 @@ source "$CURRENT_DIR/shared.sh"
 # `tmux 1.9` => `19`
 # `1.9a`     => `19`
 get_digits_from_string() {
-	local string="$1"
-	local only_digits="$(echo "$string" | tr -dC '[:digit:]')"
-	echo "$only_digits"
-}
-
-tmux_version_int() {
-	local tmux_version_string=$(tmux -V)
-	echo "$(get_digits_from_string "$tmux_version_string")"
-}
-
-unsupported_version_message() {
-	echo "Error, Tmux version unsupported! Please install Tmux version $SUPPORTED_VERSION or greater!"
-}
-
-exit_if_unsupported_version() {
-	local current_version="$1"
-	local supported_version="$2"
-	if [ "$current_version" -lt "$supported_version" ]; then
-		display_message "$(unsupported_version_message)"
-		exit 1
-	fi
+	echo "$1" | tr -dC '[:digit:]'
 }
 
 main() {
 	local supported_version_int="$(get_digits_from_string "$SUPPORTED_VERSION")"
-	local current_version_int="$(tmux_version_int)"
-	exit_if_unsupported_version "$current_version_int" "$supported_version_int"
+	local current_version_int="$(get_digits_from_string "$(tmux -V)")"
+	if [ "$current_version_int" -lt "$supported_version_int" ]; then
+		display_message "Error, Tmux version unsupported! Please install Tmux version $SUPPORTED_VERSION or greater!"
+		exit 1
+	fi
 }
 main
