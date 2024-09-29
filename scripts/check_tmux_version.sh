e065328e423287a6161069fa9424da1e853399e6
Phil Hindman <pahindman@user.noreply.github.com>
2024-09-28 23:28:32 -0500

    Use shared.sh and variables.sh in check_tmux_version.h
    - Remove duplicate definitions of get_tmux_option and display_message.
    - Remove unused UNSUPPORTED_MESSAGE.
    - Use SUPPORTED_VERSION from variables.sh.

--- scripts/check_tmux_version.sh
+++ scripts/check_tmux_version.sh
@@ -1,43 +1,9 @@
 #!/usr/bin/env bash
 
-VERSION="$1"
-UNSUPPORTED_MSG="$2"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-get_tmux_option() {
-	local option=$1
-	local default_value=$2
-	local option_value=$(tmux show-option -gqv "$option")
-	if [ -z "$option_value" ]; then
-		echo "$default_value"
-	else
-		echo "$option_value"
-	fi
-}
-
-# Ensures a message is displayed for 5 seconds in tmux prompt.
-# Does not override the 'display-time' tmux option.
-display_message() {
-	local message="$1"
-
-	# display_duration defaults to 5 seconds, if not passed as an argument
-	if [ "$#" -eq 2 ]; then
-		local display_duration="$2"
-	else
-		local display_duration="5000"
-	fi
-
-	# saves user-set 'display-time' option
-	local saved_display_time=$(get_tmux_option "display-time" "750")
-
-	# sets message display time to 5 seconds
-	tmux set-option -gq display-time "$display_duration"
-
-	# displays message
-	tmux display-message "$message"
-
-	# restores original 'display-time' value
-	tmux set-option -gq display-time "$saved_display_time"
-}
+source "$CURRENT_DIR/variables.sh"
+source "$CURRENT_DIR/shared.sh"
 
 # this is used to get "clean" integer version number. Examples:
 # `tmux 1.9` => `19`
@@ -54,11 +20,7 @@ tmux_version_int() {
 }
 
 unsupported_version_message() {
-	if [ -n "$UNSUPPORTED_MSG" ]; then
-		echo "$UNSUPPORTED_MSG"
-	else
-		echo "Error, Tmux version unsupported! Please install Tmux version $VERSION or greater!"
-	fi
+	echo "Error, Tmux version unsupported! Please install Tmux version $SUPPORTED_VERSION or greater!"
 }
 
 exit_if_unsupported_version() {
@@ -71,7 +33,7 @@ exit_if_unsupported_version() {
 }
 
 main() {
-	local supported_version_int="$(get_digits_from_string "$VERSION")"
+	local supported_version_int="$(get_digits_from_string "$SUPPORTED_VERSION")"
 	local current_version_int="$(tmux_version_int)"
 	exit_if_unsupported_version "$current_version_int" "$supported_version_int"
 }
