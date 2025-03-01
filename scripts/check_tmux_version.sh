8b8a5b169941025712ec1c8c14b54d822c8c8c78
Phil Hindman <pahindman@user.noreply.github.com>
2025-03-01 17:01:39 -0600

    Simplify get_digits_from_string

--- scripts/check_tmux_version.sh
+++ scripts/check_tmux_version.sh
@@ -9,9 +9,7 @@ source "$CURRENT_DIR/shared.sh"
 # `tmux 1.9` => `19`
 # `1.9a`     => `19`
 get_digits_from_string() {
-	local string="$1"
-	local only_digits="$(echo "$string" | tr -dC '[:digit:]')"
-	echo "$only_digits"
+	echo "$1" | tr -dC '[:digit:]'
 }
 
 tmux_version_int() {
