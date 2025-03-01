d635fb8957aea12b4ebde8a566e9ff5fdb495430
Phil Hindman <pahindman@user.noreply.github.com>
2025-03-01 17:07:03 -0600

    Inline exit_if_unsupported_version

--- scripts/check_tmux_version.sh
+++ scripts/check_tmux_version.sh
@@ -16,18 +16,12 @@ unsupported_version_message() {
 	echo "Error, Tmux version unsupported! Please install Tmux version $SUPPORTED_VERSION or greater!"
 }
 
-exit_if_unsupported_version() {
-	local current_version="$1"
-	local supported_version="$2"
-	if [ "$current_version" -lt "$supported_version" ]; then
-		display_message "$(unsupported_version_message)"
-		exit 1
-	fi
-}
-
 main() {
 	local supported_version_int="$(get_digits_from_string "$SUPPORTED_VERSION")"
 	local current_version_int="$(get_digits_from_string "$(tmux -V)")"
-	exit_if_unsupported_version "$current_version_int" "$supported_version_int"
+	if [ "$current_version_int" -lt "$supported_version_int" ]; then
+		display_message "$(unsupported_version_message)"
+		exit 1
+	fi
 }
 main
